7213e383fa2a9a565b1a93ececd4f19f46eabc9a
Sergey Filatov <RI-541215>
2019-03-14 16:19:06 +0500

    Работа с файлом GitTest.h

--- GitTest/GitTest.h
+++ GitTest/GitTest.h
@@ -1,1 +1,9 @@
 #pragma once
+
+#ifdef FILATOVDLL_EXPORTS
+#define FILATOVDLL_API __declspec(dllexport)
+#else
+#define FILATOVDLL_API __declspec(dllimport)
+#endif
+
+extern "C" FILATOVDLL_API int MyInfo();
